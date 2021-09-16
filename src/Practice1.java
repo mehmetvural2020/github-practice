65ae3c25acf45b2cd851e1bb1dd76feff969f664
mv <mehmetvurall@gmail.com>
2021-09-17 00:52:44 +0700

    Practice2.java file added.

--- src/Practice1.java
+++ src/Practice1.java
@@ -1,5 +1,6 @@
 public class Practice1 {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Sprint 1");
     }
 }
---
+++ src/Practice2.java
@@ -0,0 +1,8 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 3;
+        int sum = x + y;
+        System.out.println("x + y = " + sum);
+    }
+}
